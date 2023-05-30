ebc9a61bef76d0086d2cf9fd4a9affc5ceea611b
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2023-05-30 15:39:38 +0300

    change counter reducer

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit"
-import counterReducer from "../features/counter/counterSlice"
+import gameCounterReducer from "../features/gameCounter/gameCounterSlice"
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    counter: gameCounterReducer,
   },
 })
 
